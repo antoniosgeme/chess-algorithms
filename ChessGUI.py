168c7617672ba168ca1f60f208b96e50a84e63db
Antonios <=>
2021-03-22 22:08:12 -0400

    GUI is ready for the white pieces

--- ChessGUI.py
+++ ChessGUI.py
@@ -30,51 +30,56 @@ def main():
     running = True
     while running:
         for e in p.event.get():
-                if e.type == p.QUIT:
-                     p.display.quit()
-                     running = False
-                     sys.exit()
-                elif e.type == p.MOUSEBUTTONDOWN:
-                    if not game_over:
-                        location = p.mouse.get_pos()
-                        col = location[0] // square_size
-                        row = location[1] // square_size
-                        if squares_selected == (row,col):
-                            squares_selected = ()
-                            last_two_player_clicks = []   
-                        else:
-                            squares_selected = (row,col)
-                            last_two_player_clicks.append(squares_selected) 
-                        if len(last_two_player_clicks) == 2: #After second click
-                            start_square =  (7 - last_two_player_clicks[0][0]) * 8 + last_two_player_clicks[0][1]
-                            end_square = (7 - last_two_player_clicks[1][0]) * 8 + last_two_player_clicks[1][1]
-                            move = ChessEngine.chess.Move(start_square,end_square)
-                            if move in game_state.board.legal_moves:
-                                game_state.board.push(move)
-                                move_made = True
-                                squares_selected = ()
-                                last_two_player_clicks = []
-                            if not move_made:
-                                last_two_player_clicks = [squares_selected]
-                        
-                elif e.type ==  p.KEYDOWN and game_state.board.move_stack:
-                    if e.key == p.K_z: # Undo when z is pressed
-                        game_state.board.pop()
-                        move_made = True
-                    if e.key == p.K_r:
-                        game_state.board.reset()
+            if e.type == p.QUIT:
+                 p.display.quit()
+                 running = False
+                 sys.exit()
+            elif e.type == p.MOUSEBUTTONDOWN:
+                if not game_over:
+                    location = p.mouse.get_pos()
+                    col = location[0] // square_size
+                    row = location[1] // square_size
+                    if squares_selected == (row,col):
                         squares_selected = ()
-                        last_two_player_clicks = []
-                        move_made = False
-                        game_over = False
-            
-        draw_current_board(screen,game_state)
+                        last_two_player_clicks = []   
+                    else:
+                        squares_selected = (row,col)
+                        last_two_player_clicks.append(squares_selected)
+                    print(squares_selected)
+                    print(last_two_player_clicks)
+                    if len(last_two_player_clicks) == 2: #After second click
+                        start_square =  (7 - last_two_player_clicks[0][0]) * 8 + last_two_player_clicks[0][1]
+                        end_square = (7 - last_two_player_clicks[1][0]) * 8 + last_two_player_clicks[1][1]
+                        move = ChessEngine.chess.Move(start_square,end_square)
+                        if move in game_state.board.legal_moves:
+                            game_state.board.push(move)
+                            move_made = True
+                            squares_selected = ()
+                            last_two_player_clicks = []
+                        if not move_made:
+                            last_two_player_clicks = [squares_selected]
+                    
+            elif e.type ==  p.KEYDOWN and game_state.board.move_stack:
+                if e.key == p.K_z: # Undo when z is pressed
+                    game_state.board.pop()
+                    move_made = True
+                if e.key == p.K_r:
+                    game_state.board.reset()
+                    squares_selected = ()
+                    last_two_player_clicks = []
+                    move_made = False
+                    game_over = False
+                
+        if move_made:
+            move_made = False
+        draw_current_board(screen,game_state,squares_selected)
         clock.tick(fps)
         p.display.flip()
         
     
-def draw_current_board(screen,game_state):
+def draw_current_board(screen,game_state,squares_selected):
      draw_empty_board(screen)
+     highlight_squares(screen,game_state,squares_selected)
      draw_pieces(screen,game_state)
      
 def draw_pieces(screen,game_state):
@@ -95,15 +100,24 @@ def draw_pieces(screen,game_state):
 def highlight_squares(screen,game_state,squares_selected):
     if squares_selected != ():
         r,c = squares_selected
-        if game_state.board[r][c][0] == ("w" if gs.whiteToMove else "b"):
+        start_square = (7 - r) * 8 + c
+        if game_state.board.color_at(start_square) == game_state.board.turn:
             s = p.Surface((square_size,square_size))
             s.set_alpha(100)
             s.fill(p.Color("blue"))
             screen.blit(s,(c*square_size,r*square_size))
             s.fill(p.Color("yellow"))
-            for move in validMoves:
-                if move.startRow == r and move.startCol == c:
-                    screen.blit(s,(move.endCol*SQ_SIZE,move.endRow*SQ_SIZE))
+            
+            for end_square in range(63):
+                move = ChessEngine.chess.Move(start_square,end_square)
+                if move in game_state.board.legal_moves:
+                    square = move.to_square
+                    c = square % board_dimension
+                    r = board_dimension - 1 - square // board_dimension
+                    screen.blit(s,(c*square_size,r*square_size))
+                
+
+                
     
     
 
