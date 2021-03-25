e684cff9785e068384cdeaeb808e45bba91cadf6
Antonios <=>
2021-03-24 21:27:51 -0400

    You can now play with the black pieces

--- ChessGUI.py
+++ ChessGUI.py
@@ -8,12 +8,10 @@ square_size = board_height // board_dimension
 fps = 15
 images = {}
 
-
-
-
 def main():
-    
-        
+    choice = input("Would you like to play with the white pieces or the black pieces? (W/B) : ")
+    HumanVsHuman(choice.upper() =="W")
+def HumanVsHuman(play_with_white: bool):
     p.init()
     screen = p.display.set_mode((board_width,board_height))
     clock = p.time.Clock()
@@ -22,8 +20,7 @@ def main():
     load_lichess_images()
     squares_selected = ()
     last_two_player_clicks = []
-    
-    
+        
     move_made = False
     game_over = False
     running = True
@@ -44,11 +41,9 @@ def main():
                     else:
                         squares_selected = (row,col)
                         last_two_player_clicks.append(squares_selected)
-                    print(squares_selected)
-                    print(last_two_player_clicks)
                     if len(last_two_player_clicks) == 2: #After second click
-                        start_square =  (7 - last_two_player_clicks[0][0]) * 8 + last_two_player_clicks[0][1]
-                        end_square = (7 - last_two_player_clicks[1][0]) * 8 + last_two_player_clicks[1][1]
+                        start_square =  convert_square(last_two_player_clicks[0],play_with_white)
+                        end_square = convert_square(last_two_player_clicks[1],play_with_white)
                         move = ChessEngine.chess.Move(start_square,end_square)
                         if move in game_state.board.legal_moves:
                             game_state.board.push(move)
@@ -56,8 +51,7 @@ def main():
                             squares_selected = ()
                             last_two_player_clicks = []
                         if not move_made:
-                            last_two_player_clicks = [squares_selected]
-                    
+                            last_two_player_clicks = [squares_selected]   
             elif e.type ==  p.KEYDOWN and game_state.board.move_stack:
                 if e.key == p.K_z: # Undo when z is pressed
                     game_state.board.pop()
@@ -67,12 +61,10 @@ def main():
                     squares_selected = ()
                     last_two_player_clicks = []
                     move_made = False
-                    game_over = False
-                
+                    game_over = False  
         if move_made:
             move_made = False
-        draw_current_board(screen,game_state,squares_selected)
-        
+        draw_current_board(screen,game_state,squares_selected,play_with_white)
         if game_state.board.is_checkmate():
             game_over = True
             if game_state.board.turn:   
@@ -83,15 +75,14 @@ def main():
             game_over = True
             drawText(screen,"Stalemate")
         clock.tick(fps)
-        p.display.flip()
-        
+        p.display.flip()       
     
-def draw_current_board(screen,game_state,squares_selected):
+def draw_current_board(screen,game_state,squares_selected,play_with_white):
      draw_empty_board(screen)
-     highlight_squares(screen,game_state,squares_selected)
-     draw_pieces(screen,game_state)
+     highlight_squares(screen,game_state,squares_selected,play_with_white)
+     draw_pieces(screen,game_state,play_with_white)
      
-def draw_pieces(screen,game_state):
+def draw_pieces(screen,game_state,play_with_white):
     for color in [ChessEngine.chess.WHITE, ChessEngine.chess.BLACK]:
         for piece in range(1,7):
             if color:
@@ -102,14 +93,15 @@ def draw_pieces(screen,game_state):
             for square in list(game_state.board.pieces(piece,color)):
                 c = square % board_dimension
                 r = board_dimension - 1 - square // board_dimension
-                screen.blit(images[title],p.Rect(c*square_size,r*square_size,square_size,square_size))
-
-                
+                if not play_with_white:
+                    c = 7 - c
+                    r = 7 - r
+                screen.blit(images[title],p.Rect(c*square_size,r*square_size,square_size,square_size))         
             
-def highlight_squares(screen,game_state,squares_selected):
+def highlight_squares(screen,game_state,squares_selected,play_with_white):
     if squares_selected != ():
         r,c = squares_selected
-        start_square = (7 - r) * 8 + c
+        start_square = convert_square(squares_selected,play_with_white)
         if game_state.board.color_at(start_square) == game_state.board.turn:
             s = p.Surface((square_size,square_size))
             s.set_alpha(100)
@@ -123,13 +115,11 @@ def highlight_squares(screen,game_state,squares_selected):
                     square = move.to_square
                     c = square % board_dimension
                     r = board_dimension - 1 - square // board_dimension
-                    screen.blit(s,(c*square_size,r*square_size))
-                
-
-                
+                    if not play_with_white:
+                        c = 7 - c
+                        r = 7 - r
+                    screen.blit(s,(c*square_size,r*square_size))    
     
-    
-
 def load_lichess_images():
     pieces = ["wP","wR","wN","wB","wK","wQ","bP","bR","bN","bB","bK","bQ"]
     for piece in pieces:
@@ -150,6 +140,15 @@ def drawText(screen,text):
     screen.blit(textObject,textLocation)
     textObject = font.render(text,0,p.Color("black"))
     screen.blit(textObject,textLocation.move(2,2))
+
+
+    
+def convert_square(squares_selected,play_with_white):
+    if play_with_white:
+        return  ((7 - squares_selected[0]) * 8 + 
+                        squares_selected[1])
+    else:
+        return (8 * squares_selected[0] + (7-squares_selected[1]))
 
 if __name__ == "__main__":
     main()
